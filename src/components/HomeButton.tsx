b30745a1247403c7a3365b1caf34f6ca1e18840c
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-08-22 02:16:56 +0530

    logo and ui update

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,7 +1,7 @@
 const HomeButton = () => {
   return (
-    <button className="sm:text-xl font-semibold sm:ml-2 p-3 sm:p-4 rounded-md  bg-gradient-to-b from-slate-900 to-slate-800 hover:from-slate-800 hover:to-slate-900">
-      Create your account
+    <button className="sm:text-xl font-semibold sm:ml-2 p-3 sm:p-4 rounded-md  bg-gradient-to-b from-zinc-900 to-zinc-800 hover:from-zinc-800 hover:to-zinc-900">
+      Create an account
     </button>
   );
 };
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,12 +4,12 @@ import HomeButton from "../components/HomeButton";
 const Home = () => {
   return (
     <div className="h-screen flex flex-col gap-4 justify-center items-center">
+      <img src="/logo.png" alt="" className="w-14 sm:w-24" />
       <h1 className="text-3xl sm:text-5xl font-bold text-center">
         CipherVault
       </h1>
       <p className="sm:mx-7 mb-4 sm:mb-7 sm:text-2xl text-center">
-        A crypto wallet that helps you manage your solana and ethereum assets in
-        one place securely.
+        Your Crypto, Your Vault
       </p>
       <Link to="/generate">
         <HomeButton />
