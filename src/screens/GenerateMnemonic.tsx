5a1e394d56528a152c59f084018a28f33657b417
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-08-22 23:36:56 +0530

    generate page ui

--- src/screens/GenerateMnemonic.tsx
+++ src/screens/GenerateMnemonic.tsx
@@ -1,25 +1,39 @@
 import { generateMnemonic } from "bip39";
 import { useState } from "react";
+import Footer from "../components/Footer";
 
 const GenerateMnemonic = () => {
   const [mnemonic, setMnemonic] = useState("");
 
   return (
-    <div>
+    <div className="h-screen flex flex-col items-center text-center">
       <div>
+        <h1 className="mt-24 sm:mb-2 text-2xl sm:text-4xl lg:text-5xl font-bold ">
+          Generate a Secret Recovery Phrase
+        </h1>
+        <p className="sm:text-xl lg:text-2xl text-zinc-400">
+          Keep this phrase at a safe place
+        </p>
+      </div>
+      <div className="mt-7">
         <button
           onClick={function () {
             const mn = generateMnemonic();
             setMnemonic(mn);
           }}
+          className="bg-zinc-500 text-black px-4 py-2 lg:px-5 lg:py-3 rounded-lg"
         >
           Create Seed Phrase
         </button>
       </div>
-
       <div>
-        <input type="text" value={mnemonic} className="w-full" />
+        <input
+          type="text"
+          value={mnemonic}
+          className="w-full bg-[#111111] text-white"
+        />
       </div>
+      <Footer />
     </div>
   );
 };
