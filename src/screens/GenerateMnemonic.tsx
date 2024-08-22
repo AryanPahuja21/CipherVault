c706e1911058e6dbf509ffcc43e038bade8214cf
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-08-23 01:00:16 +0530

    mnemonic page ready

--- src/screens/GenerateMnemonic.tsx
+++ src/screens/GenerateMnemonic.tsx
@@ -2,9 +2,12 @@ import { generateMnemonic } from "bip39";
 import { useState } from "react";
 import Footer from "../components/Footer";
 import { MdContentCopy } from "react-icons/md";
+import { FaWallet } from "react-icons/fa";
 
 const GenerateMnemonic = () => {
   const [mnemonic, setMnemonic] = useState("");
+
+  const [copied, setCopied] = useState(false);
 
   return (
     <div className="h-screen flex flex-col items-center text-center">
@@ -21,6 +24,7 @@ const GenerateMnemonic = () => {
           onClick={function () {
             const mn = generateMnemonic();
             setMnemonic(mn);
+            setCopied(false);
           }}
           className="bg-zinc-500 hover:bg-zinc-400 text-black px-4 py-2 lg:px-5 lg:py-3 rounded-lg"
         >
@@ -34,7 +38,10 @@ const GenerateMnemonic = () => {
           }}
           className=""
         >
-          <div className="mt-10 border border-zinc-400 rounded-md bg-zinc-800">
+          <div
+            className="mt-10 border border-zinc-400 rounded-md bg-zinc-800"
+            onClick={() => setCopied(true)}
+          >
             <div className="grid grid-cols-4 gap-4 p-4">
               {mnemonic.split(" ").map((word, index) => {
                 return (
@@ -49,11 +56,34 @@ const GenerateMnemonic = () => {
             </div>
             <div className="px-5 py-3 flex items-center text-zinc-400">
               <MdContentCopy />
-              <p className="mx-2">Click anywhere to copy</p>
+              {
+                <p className="mx-2">
+                  {copied ? "Copied to clipboard!" : "Click anywhere to copy"}
+                </p>
+              }
             </div>
           </div>
         </button>
       )}
+      <div>
+        {mnemonic && (
+          <div className="flex flex-col items-center">
+            <p className="mt-10 text-zinc-400">
+              This seed phrase is the only way to recover your wallet. Keep it
+              safe and do not share it with anyone.
+            </p>
+            <button
+              onClick={function () {
+                // Redirect to the wallet page
+              }}
+              className="mt-4 bg-zinc-300 hover:bg-zinc-200 text-black px-4 py-2 lg:px-5 lg:py-3 rounded-lg flex justify-center items-center gap-2"
+            >
+              Continue to Wallet
+              <FaWallet />
+            </button>
+          </div>
+        )}
+      </div>
 
       <Footer />
     </div>
