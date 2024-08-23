051eb04125e8cb63d385a432be8066d58332ad4c
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-08-23 21:12:41 +0530

    header logout added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
+import { TbLogout } from "react-icons/tb";
+
 const Header = () => {
+  const logout = () => {
+    localStorage.removeItem("mnemonic");
+    localStorage.removeItem("wallet");
+    window.location.reload();
+  };
+
   return (
     <div className="flex justify-center">
       <div className="w-[60vw] flex flex-col justify-center">
-        <div className="flex items-center py-6">
-          <img src="/logo.png" alt="" className="w-10" />
-          <h1 className="text-3xl font-semibold">CipherVault</h1>
+        <div className="flex justify-between items-center py-6">
+          <div className="flex items-center">
+            <img src="/logo.png" alt="" className="w-10" />
+            <h1 className="text-3xl font-semibold">CipherVault</h1>
+          </div>
+          <div className="flex">
+            <button onClick={logout}>
+              <TbLogout className="w-8 h-full" />
+            </button>
+          </div>
         </div>
         <span className="w-full h-0.5 bg-zinc-500 mx-auto"></span>
       </div>
