43ae78c52f4f21bf3ed7568794fbb64def64e44d
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-08-23 21:56:31 +0530

    new wallet dialog completed

--- src/components/NewWalletDialog.tsx
+++ src/components/NewWalletDialog.tsx
@@ -1,8 +1,23 @@
+import wallets from "../utils/wallets.json";
+
 const NewWalletDialog = () => {
   return (
-    <div className="p-4 border border-zinc-200 bg-zinc-900">
-      <h1>New Solana Wallet</h1>
-      <h1>New Ethereum Wallet</h1>
+    <div className="pt-8 p-4 border border-zinc-200 bg-zinc-900">
+      {wallets.map((wallet) => {
+        return (
+          <div
+            key={wallet.id}
+            className="w-80 h-20 flex items-center p-4 border border-zinc-300 rounded-md bg-zinc-800 hover:bg-zinc-700 mb-4 cursor-pointer"
+          >
+            <img src={wallet.icon} alt="" className="w-7" />
+            <div className="flex-1">
+              <h1 className="text-xl text-center font-semibold">
+                {wallet.name}
+              </h1>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
