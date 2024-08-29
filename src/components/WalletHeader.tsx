1b71996f9f0e623f047638b6da5cf026fd440ffb
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-08-30 02:26:15 +0530

    multiple wallets accessible

--- src/components/WalletHeader.tsx
+++ src/components/WalletHeader.tsx
@@ -1,15 +1,27 @@
 import walletDetails from "../utils/wallets.json";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
-const WalletHeader = () => {
+const WalletHeader = ({
+  walletType,
+  index,
+}: {
+  walletType: string;
+  index: number;
+}) => {
   return (
     <div className="w-full mx-auto bg-zinc-900 py-4 rounded-t-md drop-shadow-lg flex items-center gap-4 px-5">
       <img
-        src={walletDetails[0].icon}
+        src={
+          walletType === "Solana"
+            ? walletDetails[0].icon
+            : walletDetails[1].icon
+        }
         className="w-11 p-2 cursor-pointer hover:bg-zinc-800 rounded-md"
       />
       <div className="w-full text-xl font-bold p-2 flex justify-between items-center cursor-pointer hover:bg-zinc-800 rounded-md">
-        <h1>Solana Wallet 1</h1>
+        <h1>
+          {walletType} Wallet {index}
+        </h1>
         <div>
           <MdKeyboardArrowDown className="w-6 h-6" />
         </div>
--- src/screens/Wallet.tsx
+++ src/screens/Wallet.tsx
@@ -10,7 +10,6 @@ import { PiCurrencyDollarLight } from "react-icons/pi";
 const Wallet = () => {
   const ethWallets = JSON.parse(localStorage.getItem("ethWallets") || "[]");
   const solWallets = JSON.parse(localStorage.getItem("solWallets") || "[]");
-  const wallets = ethWallets || solWallets;
   const [buttonText, setButtonText] = useState("Copy Public Key");
 
   const removeAllWallets = () => {
@@ -48,62 +47,110 @@ const Wallet = () => {
             </button>
           </div>
           <div className="mt-4">
-            {wallets && (
-              <div className="mx-auto max-w-[450px] py-4 rounded-md mb-4 ">
-                <header>
-                  <WalletHeader />
-                </header>
-                <main className="bg-zinc-800 pt-14 p-7 rounded-b-md">
-                  <div className="flex flex-col gap-7 justify-between items-center">
-                    <h2 className="mx-auto text-4xl font-bold">$0.0</h2>
-                    <div className="flex gap-5">
-                      <button className="w-20 h-20 bg-zinc-700 hover:bg-zinc-600 text-white font-semibold px-3 py-2 rounded-xl">
-                        <BsPlusLg className="w-8 h-8 mx-auto" />
-                        Receive
-                      </button>
-                      <button className="w-20 h-20 bg-zinc-700 hover:bg-zinc-600 text-white font-semibold px-3 py-2 rounded-xl">
-                        <PiPaperPlaneTilt className="w-8 h-8 mx-auto" />
-                        Send
-                      </button>
-                      <button className="w-20 h-20 bg-zinc-700 hover:bg-zinc-600 text-white font-semibold px-3 py-2 rounded-xl">
-                        <PiCurrencyDollarLight className="w-8 h-8 mx-auto" />
-                        Buy
-                      </button>
+            {solWallets.length >= 1 &&
+              solWallets.map((solWallet: any, index: number) => (
+                <div
+                  className="mx-auto max-w-[450px] py-4 rounded-md mb-4"
+                  key={solWallet.address}
+                >
+                  <header>
+                    <WalletHeader walletType="Solana" index={index + 1} />
+                  </header>
+                  <main className="bg-zinc-800 pt-14 p-7 rounded-b-md">
+                    <div className="flex flex-col gap-7 justify-between items-center">
+                      <h2 className="mx-auto text-4xl font-bold">$0.0</h2>
+                      <div className="flex gap-5">
+                        <button className="w-20 h-20 bg-zinc-700 hover:bg-zinc-600 text-white font-semibold px-3 py-2 rounded-xl">
+                          <BsPlusLg className="w-8 h-8 mx-auto" />
+                          Receive
+                        </button>
+                        <button className="w-20 h-20 bg-zinc-700 hover:bg-zinc-600 text-white font-semibold px-3 py-2 rounded-xl">
+                          <PiPaperPlaneTilt className="w-8 h-8 mx-auto" />
+                          Send
+                        </button>
+                        <button className="w-20 h-20 bg-zinc-700 hover:bg-zinc-600 text-white font-semibold px-3 py-2 rounded-xl">
+                          <PiCurrencyDollarLight className="w-8 h-8 mx-auto" />
+                          Buy
+                        </button>
+                      </div>
+                      <div className="relative group inline-block">
+                        <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-1 text-sm bg-zinc-900 text-white rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                          {ethWallets[0].address}
+                        </span>
+                        <button
+                          className="bg-neutral-700 hover:bg-neutral-600 text-white font-semibold px-4 py-2 rounded-md"
+                          onClick={handleCopy}
+                        >
+                          {buttonText}
+                        </button>
+                      </div>
                     </div>
-                    <div className="relative group inline-block">
-                      <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-1 text-sm bg-zinc-900 text-white rounded opacity-0 group-hover:opacity-100 transition-opacity">
-                        {ethWallets[0].address}
-                      </span>
-                      <button
-                        className="bg-neutral-700 hover:bg-neutral-600 text-white font-semibold px-4 py-2 rounded-md"
-                        onClick={handleCopy}
-                      >
-                        {buttonText}
-                      </button>
-                    </div>
-                  </div>
-                  <div>
-                    <div className="mt-10 flex flex-col gap-5">
-                      {solWallets && (
-                        <div className="flex justify-between items-center bg-zinc-700 hover:bg-zinc-600 cursor-pointer p-4 rounded-2xl">
-                          <div className="flex gap-4 items-center">
-                            <img
-                              src={walletDetails[0].icon}
-                              alt={walletDetails[0].name}
-                              className="w-10 h-10"
-                            />
-                            <div>
-                              <h3 className="text-xl font-bold">
-                                {walletDetails[0].name}
-                              </h3>
-                              <p className="text-zinc-400">
-                                0 {walletDetails[0].ticker}
-                              </p>
+                    <div>
+                      <div className="mt-10 flex flex-col gap-5">
+                        {solWallets && (
+                          <div className="flex justify-between items-center bg-zinc-700 hover:bg-zinc-600 cursor-pointer p-4 rounded-2xl">
+                            <div className="flex gap-4 items-center">
+                              <img
+                                src={walletDetails[0].icon}
+                                alt={walletDetails[0].name}
+                                className="w-10 h-10"
+                              />
+                              <div>
+                                <h3 className="text-xl font-bold">
+                                  {walletDetails[0].name}
+                                </h3>
+                                <p className="text-zinc-400">
+                                  0 {walletDetails[0].ticker}
+                                </p>
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      )}
-                      {ethWallets && (
+                        )}
+                      </div>
+                    </div>
+                  </main>
+                </div>
+              ))}
+            {ethWallets.length >= 1 &&
+              ethWallets.map((ethWallet: any, index: number) => (
+                <div
+                  className="mx-auto max-w-[450px] py-4 rounded-md mb-4 "
+                  key={ethWallet.address}
+                >
+                  <header>
+                    <WalletHeader walletType="Ethereum" index={index + 1} />
+                  </header>
+                  <main className="bg-zinc-800 pt-14 p-7 rounded-b-md">
+                    <div className="flex flex-col gap-7 justify-between items-center">
+                      <h2 className="mx-auto text-4xl font-bold">$0.0</h2>
+                      <div className="flex gap-5">
+                        <button className="w-20 h-20 bg-zinc-700 hover:bg-zinc-600 text-white font-semibold px-3 py-2 rounded-xl">
+                          <BsPlusLg className="w-8 h-8 mx-auto" />
+                          Receive
+                        </button>
+                        <button className="w-20 h-20 bg-zinc-700 hover:bg-zinc-600 text-white font-semibold px-3 py-2 rounded-xl">
+                          <PiPaperPlaneTilt className="w-8 h-8 mx-auto" />
+                          Send
+                        </button>
+                        <button className="w-20 h-20 bg-zinc-700 hover:bg-zinc-600 text-white font-semibold px-3 py-2 rounded-xl">
+                          <PiCurrencyDollarLight className="w-8 h-8 mx-auto" />
+                          Buy
+                        </button>
+                      </div>
+                      <div className="relative group inline-block">
+                        <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-1 text-sm bg-zinc-900 text-white rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                          {ethWallets[0].address}
+                        </span>
+                        <button
+                          className="bg-neutral-700 hover:bg-neutral-600 text-white font-semibold px-4 py-2 rounded-md"
+                          onClick={handleCopy}
+                        >
+                          {buttonText}
+                        </button>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="mt-10 flex flex-col gap-5">
                         <div className="flex justify-between items-center bg-zinc-700 hover:bg-zinc-600 cursor-pointer p-4 rounded-2xl">
                           <div className="flex gap-4 items-center">
                             <img
@@ -121,12 +168,11 @@ const Wallet = () => {
                             </div>
                           </div>
                         </div>
-                      )}
+                      </div>
                     </div>
-                  </div>
-                </main>
-              </div>
-            )}
+                  </main>
+                </div>
+              ))}
           </div>
         </div>
       </main>
